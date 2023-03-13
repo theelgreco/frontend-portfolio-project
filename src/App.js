afa65f3cf458f728f50375c78dbfc15e07bce288
Stelios Kakoliris <stelioskakoliris@Stelioss-MacBook-Pro.local>
2023-03-13 15:57:22 +0000

    created Review component file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Reviews from "./Components/Reviews.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Reviews />
+    </div>
+  );
 }
 
 export default App;
